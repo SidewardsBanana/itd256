4644c4aa9528f627217b0a3e0da7cae015fbb2d5
SidewardsBanana <jlxfallon@gmail.com>
2023-01-03 09:24:20 -0500

    02 Sql File Finished

---
+++ Chapter 1/02_build.sql
@@ -0,0 +1,38 @@
+drop table if exists master_plan;
+create table master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
+
+COPY master_plan 
+FROM 'C:\Users\jlxfa\OneDrive\cassini_data\curious_data\data\master_plan.csv' 
+WITH DELIMITER ',' HEADER CSV;
+
+/* with a schema 
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
+
+COPY import.master_plan 
+FROM 'C:\Users\jlxfa\OneDrive\cassini_data\curious_data\data\master_plan.csv' 
+WITH DELIMITER ',' HEADER CSV;
+*/
