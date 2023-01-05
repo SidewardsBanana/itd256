108cb7301d2ebfec1a6d725d00390875cdeea174
SidewardsBanana <jlxfallon@gmail.com>
2023-01-05 15:04:26 -0500

    Build.sql Done

---
+++ Chapter 1/build.sql
@@ -0,0 +1,15 @@
+drop table if exists master_plan;
+create table master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
+
+COPY import.master_plan FROM 'C:\Users\jlxfa\OneDrive\cassini_data\curious_data\data\master_plan.csv' WITH DELIMITER ',' HEADER CSV;
